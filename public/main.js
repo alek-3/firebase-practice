9c0a51ae5e118b5161e44578dd8b4b1ffc31d114
alek-3 <jaianism.of.grotius@hotmail.co.jp>
2019-02-17 00:50:39 +0900

    implement chapter3 firebase authentication

---
+++ public/main.js
@@ -0,0 +1,51 @@
+async function initAuth() {
+    const auth = firebase.auth();
+
+    const $app = document.getElementById('app');
+    const $load = document.getElementById('load');
+    const $authButton = document.getElementById('auth-button');
+
+    let loaded = false;
+    let loginUser = null;
+    auth.onAuthStateChanged(user => {
+        if(!loaded){
+            loaded = true;
+            $load.style.display = 'none';
+            $app.style.display = '';
+        }
+        loginUser = user;
+
+        if(user){
+            $authButton.innerText = 'Logout';
+        }else{
+            $authButton.innerText = 'Login';
+        }
+
+    });
+
+    $authButton.addEventListener('click', () =>{
+        if(loginUser){
+            auth.signOut().then(() => location.reload());
+            return;
+        }
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithRedirect(provider);
+    })
+
+    try {
+        const result = await auth.getRedirectResult();
+        // 次のトークンを使ってGoogle APIを呼び出せる。
+        // const token = result.credential.accessToken;
+    } catch(err){
+        alert(err.message);
+        console.error(err);
+    }
+};
+
+async function main(){
+    await initAuth();
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    main().catch(err => console.log(err));
+});
